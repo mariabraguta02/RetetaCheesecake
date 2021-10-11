83257548aa94cb91b82adfe999f2c4a28a5af16d
Maria Braguta <90184296+mariabraguta02@users.noreply.github.com>
2021-10-11 18:22:47 +0300

    Add files via upload
    În lucrarea nr.3 am utilizat la fel HTML, CSS + JavaScript.

---
+++ js/myscripts.js
@@ -0,0 +1,11 @@
+let img = document.querySelector('img');
+let btn1 = document.querySelector('#btn1');
+let btn2 = document.querySelector('#btn2');
+
+btn1.addEventListener('click', () => {
+	img.src = src='D:\Lucrari\Sem.III\Tehnologii Web\LAB.2\imagines/minion1.png'
+})
+
+btn2.addEventListener('click', () => {
+	img.src = src='D:\Lucrari\Sem.III\Tehnologii Web\LAB.2\imagines/minion2.png'
+})
